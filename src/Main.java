e9a003c661260abcc3643c7b8728157d0c42b4b0
sarmeehh <sarmasanu.alex@gmail.com>
2025-06-09 15:56:30 +0300

    update test 01

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        //Ask for user's name
         System.out.println("Enter your name: ");
         String name = scanner.nextLine();
 
-        //Create a bank account
         BankAccount bankAccount = new BankAccount(name);
         bankAccount.showMenu();
 
